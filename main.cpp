ae4a9d48677841fca2a5d71c2b7482291aadfb0a
ErianArcher <jungg1996@gmail.com>
2019-07-19 15:50:03 +0800

    整个构建DB图的框架

--- main.cpp
+++ main.cpp
@@ -1,255 +1,17 @@
 #include <iostream>
 #include <mpi.h>
 #include <sstream>
+#include <pthread.h>
 #include "entity/idset.h"
 #include "entity/read.h"
 #include "entity/k_minus_mer.h"
 #include "entity/k_mer.h"
+#include "test.h"
+#include "communicator.h"
+
+#define TRESHOLD 1024*1024*256
 
 using namespace std;
-
-string setToString(SetOfID *set) {
-    stringstream sstream;
-    sstream << "{";
-    for (auto itr: *set) {
-        sstream << itr << ", ";
-    }
-    sstream << "}";
-    return sstream.str();
-}
-
-void idSetTest() {
-    SetOfID *set1 = new SetOfID;
-    SetOfID *set2 = new SetOfID;
-    SetOfID *resset = nullptr;
-    set1->insert(hash<string>()("Hello"));
-    set1->insert(hash<string>()("world"));
-    set2->insert(hash<string>()("Hello"));
-    set2->insert(hash<string>()("erian"));
-    cout << "set1: " << setToString(set1) << "\n" <<
-         "set2: " << setToString(set2) << "\n";
-    cout << "set1 union set2: ";
-    resset = setUnion(*set1, *set2);
-    cout << setToString(resset) << "\n";
-    delete resset;
-    cout << "set1 intersect set2: ";
-    resset = setIntersect(*set1, *set2);
-    cout << setToString(resset) << "\n";
-    delete resset;
-    cout << "set1 minus set2: ";
-    resset = setMinus(*set1, *set2);
-    cout << setToString(resset) << "\n";
-    delete resset;
-
-    cout << "set1 union set2: ";
-    setUnionTo(set1, *set2);
-    cout << setToString(set1) << "\n";
-    cout << "set1 intersect set2: ";
-    setIntersectTo(set1, *set2);
-    cout << setToString(set1) << "\n";
-    cout << "set2 minus set1: ";
-    setMinusTo(set2, *set1);
-    cout << setToString(set2) << "\n";
-}
-
-void readTest() {
-    createReadDir();
-    string reads[] = {"abcdefghabcijklmnabc", "gdfeabcgvfaondfabcevob", "guabceprgvujbgorpgvfadpuoguahbabc",
-                      "abcjfoaihjgoigoreabcbug", "abcvgnedfaiosngjogrenbfoijgbnv", "fioeawrhgophnoarhgouiabc",
-                      "goierahabcfreoiaghoi"};
-    int len = 7;
-    ReadId *readIds = new ReadId[len];
-    for (int i = 0; i < len; ++i) {
-        ReadId *readId = new ReadId;
-        createRead(reads[i], readId);
-        readIds[i] = *readId;
-        delete readId;
-    }
-
-    cout << "Test getRead:" << endl;
-    string *oread = new string;
-    for (int i = 0; i < len; ++i) {
-        getRead(oread, readIds[i]);
-        cout << "Read id #" << readIds[i] << ": " << *oread << endl;
-    }
-
-    cout << "Test read alteration:" << endl;
-    int alteredLen = 1;
-    int no = 0;
-    ReadId alteredReadId = readIds[no];
-    stringstream ss;
-    ss << "b" << reads[no];
-    enlargeRead(ss.str(), alteredLen, alteredReadId, ALTER_HEAD);
-    getRead(oread, alteredReadId);
-    cout << "Read id #" << alteredReadId << ": " << *oread << endl;
-    cutRead(reads[no], alteredLen, alteredReadId, ALTER_HEAD);
-    getRead(oread, alteredReadId);
-    cout << "Read id #" << alteredReadId << ": " << *oread << endl;
-
-    ss.str("");
-    ss << reads[no] << "a";
-    enlargeRead(ss.str(), alteredLen, alteredReadId, ALTER_TAIL);
-    getRead(oread, alteredReadId);
-    cout << "Read id #" << alteredReadId << ": " << *oread << endl;
-    cutRead(reads[no], alteredLen, alteredReadId, ALTER_TAIL);
-    getRead(oread, alteredReadId);
-    cout << "Read id #" << alteredReadId << ": " << *oread << endl;
-
-    ss.str("");
-    ss << "b" << reads[no] << "a";
-    enlargeRead(ss.str(), 2, alteredReadId, ALTER_BOTH);
-    getRead(oread, alteredReadId);
-    cout << "Read id #" << alteredReadId << ": " << *oread << endl;
-    cutRead(reads[no], 2, alteredReadId, ALTER_BOTH);
-    getRead(oread, alteredReadId);
-    cout << "Read id #" << alteredReadId << ": " << *oread << endl;
-
-    KMERPOS_t flag;
-    if ((flag = getKMerPositionInRead("abc", reads[no])) != (START_KMER | INCLUDE_KMER | END_KMER)) {
-        cout << "Mismatch: " << reads[no] << "; flag: " << flag << endl;
-    }
-
-    if ((flag = getKMerPositionInRead("abc", reads[6])) != (INCLUDE_KMER)) {
-        cout << "Mismatch: " << reads[6] << "; flag: " << flag << endl;
-    }
-
-    if ((flag = getKMerPositionInRead("abc", reads[5])) != (END_KMER)) {
-        cout << "Mismatch: " << reads[5] << "; flag: " << flag << endl;
-    }
-
-    if ((flag = getKMerPositionInRead("abc", reads[4])) != (START_KMER)) {
-        cout << "Mismatch: " << reads[4] << "; flag: " << flag << endl;
-    }
-
-    if ((flag = getKMerPositionInRead("abc", reads[3])) != (START_KMER | INCLUDE_KMER)) {
-        cout << "Mismatch: " << reads[3] << "; flag: " << flag << endl;
-    }
-
-    if ((flag = getKMerPositionInRead("abc", reads[2])) != (INCLUDE_KMER | END_KMER)) {
-        cout << "Mismatch: " << reads[2] << "; flag: " << flag << endl;
-    }
-
-    if ((flag = getKMerPositionInRead("abc", reads[1])) != (INCLUDE_KMER)) {
-        cout << "Mismatch: " << reads[1] << "; flag: " << flag << endl;
-    }
-
-    delete oread;
-    delete[] readIds;
-}
-
-string vertexToString(Vertex *v) {
-    stringstream sstream;
-    sstream << "{";
-    sstream << "VertexId: " << v->id << ", ";
-    sstream << "InEdges:(" << v->inDegree << ") " << setToString(v->inKMer) << ", ";
-    sstream << "OutEdges:(" << v->outDegree << ") " << setToString(v->outKMer) << "";
-    sstream << "}";
-    return sstream.str();
-}
-
-void vertexTest() {
-    VertexList *vertexList = new VertexList;
-    EdgeId eids[] = {1, 2, 3, 4};
-    VertexId *vIds = new VertexId[4];
-    char *kMinusMers[] = {"hello", "world", "erian", "isolation"};
-    addVertex(vertexList, kMinusMers[0], (EdgeId) 0, eids[0], HEAD_VERTEX);
-    if (addVertex(vertexList, kMinusMers[0], (EdgeId) 0, eids[3], HEAD_VERTEX) != MULTI_OUT_DEGREE)
-        cout << "Multi out degree fails" << endl;
-    addVertex(vertexList, kMinusMers[1], eids[2], eids[3], IN_VERTEX);
-    if (addVertex(vertexList, kMinusMers[1], eids[0], eids[1], IN_VERTEX) != MULTI_BOTH_DEGREE)
-        cout << "Multi both degree fails" << endl;
-    addVertex(vertexList, kMinusMers[2], eids[1], (EdgeId) 0, TAIL_VERTEX);
-    if (addVertex(vertexList, kMinusMers[2], eids[0], (EdgeId) 0, TAIL_VERTEX) != MULTI_IN_DEGREE)
-        cout << "Multi in degree fails" << endl;
-    addVertex(vertexList, kMinusMers[3], &vIds[3], (EdgeId) 0, (EdgeId) 0, ISOLATE_VERTEX);
-    for (int i = 0; i < 4; ++i) {
-        VertexId vId = hash<string>()(kMinusMers[i]);
-        if (i == 3 && vId != vIds[i]) cout << "Error in addVertex() for fetching vertex id." << endl;
-        vIds[i] = vId;
-        cout << vertexToString(vertexList->at(vId)) << endl;
-    }
-
-    cout << "=============================" << endl;
-    Vertex *op = vertexList->at(vIds[3]);
-    addInEdge(vertexList, vIds[3], eids[0]);
-    cout << vertexToString(op) << endl;
-    removeInEdge(vertexList, vIds[3], eids[0]);
-    cout << vertexToString(op) << endl;
-    addOutEdge(vertexList, vIds[3], eids[0]);
-    cout << vertexToString(op) << endl;
-    removeOutEdge(vertexList, vIds[3], eids[0]);
-    cout << vertexToString(op) << endl;
-
-    cout << "=============================" << endl;
-    removeVertex(vertexList, vIds[3]);
-    for (auto kv: *vertexList) {
-        cout << vertexToString(kv.second) << endl;
-    }
-
-    delete vertexList;
-    delete[] vIds;
-}
-
-string edgeToString(Edge *edge) {
-    stringstream sstream;
-    sstream << "{";
-    sstream << "EdgeId: " << edge->id << ", ";
-    sstream << "Value: " << edge->value << ", ";
-    sstream << "AddtionValue:(" << edge->isZ << ")(" << edge->sizeOfAdditionValue << ") "
-    << (edge->additionValue?edge->additionValue:"") << ", ";
-    sstream << "Connection: " << edge->sourceKMinusMerId << "->" << edge->sinkKMinusMerId << ", ";
-    sstream << "Multiplicity: " << edge->multiplicity << ", ";
-    sstream << "AvailPassTime: " << edge->availPassTime << ", ";
-    sstream << "endHerePathSet: " << setToString(edge->endHerePathSet) << ", ";
-    sstream << "startFromHerePathSet: " << setToString(edge->startFromHerePathSet) << ", ";
-    sstream << "includeThisPathSet: " << setToString(edge->includeThisPathSet);
-    return sstream.str();
-}
-
-void edgeTest() {
-    VertexId vIds[] = {1, 2, 3, 4};
-    ReadId rIds[] = {1, 2, 3, 4};
-    char *kMers[] = {"hello", "world", "erian", "multireads"};
-    EdgeList *edgeList = new EdgeList;
-    EdgeId *eIds = new EdgeId[4];
-    addNewEdge(edgeList, kMers[0], vIds[1], vIds[0], rIds[0], INCLUDE_KMER);
-    addNewEdge(edgeList, kMers[1], vIds[0], vIds[2], rIds[1], START_KMER);
-    addNewEdge(edgeList, kMers[1], vIds[0], vIds[2], rIds[2], END_KMER);
-    addNewEdge(edgeList, kMers[2], vIds[0], vIds[3], rIds[2], END_KMER);
-    addNewEdge(edgeList, kMers[3], &eIds[3], vIds[1], vIds[3], rIds[3], START_KMER | INCLUDE_KMER | END_KMER);
-
-    for (int i = 0; i < 4; ++i) {
-        EdgeId eId= hash<string>()(kMers[i]);
-        if (i == 3 && eId != eIds[i]) cout << "Error in addNewEdge() for fetching edge id." << endl;
-        eIds[i] = eId;
-        cout << edgeToString(edgeList->at(eId)) << endl;
-    }
-
-    Edge *op = edgeList->at(eIds[3]);
-    EdgeId *zeId = new EdgeId;
-    addReadPathTo(edgeList, eIds[3], rIds[1], START_KMER);
-    addNewZEdge(edgeList, op->value, "z", zeId, op->sourceKMinusMerId, vIds[2],
-                op->endHerePathSet, op->startFromHerePathSet,op->includeThisPathSet);
-    cout << edgeToString(edgeList->at(*zeId)) << endl;
-    addNewZEdge(edgeList, op->value, "z", zeId, op->sourceKMinusMerId, vIds[2],
-                op->startFromHerePathSet, op->startFromHerePathSet,op->includeThisPathSet);
-    cout << edgeToString(edgeList->at(*zeId)) << endl;
-    cout << "=======removing edge from z edge ===============" << endl;
-    removeReadPathFrom(edgeList, *zeId, rIds[1], END_KMER);
-    cout << edgeToString(edgeList->at(*zeId)) << endl;
-    removeEdge(edgeList, *zeId);
-    cout << "==================================" << endl;
-    for (auto kv : *edgeList) {
-        cout << edgeToString(kv.second) << endl;
-    }
-    reduceAvailPassTime(edgeList, eIds[3]);
-    if (!checkAvailPassTimeNonZero(edgeList, eIds[3])) cout << "1Edge #" << eIds[3] << "cannot be passed anymore." << endl;
-    increaseAvailPassTime(edgeList, eIds[3]);
-    if (!checkAvailPassTimeNonZero(edgeList, eIds[3])) cout << "2Edge #" << eIds[3] << "cannot be passed anymore." << endl;
-
-    delete[](eIds);
-    delete edgeList;
-}
 
 inline size_t getId(string value) {
     return hash<string>()(value);
@@ -257,11 +19,10 @@ inline size_t getId(string value) {
 
 string getNextRead(int rank, int world_size, int read_num, string filepath, size_t *readpos);
 
-void requestRankToStoreRead(int currank, int tarrank, const char *filename, size_t pos);
-
 int main(int argc, char** argv) {
     // Initialize the MPI environment
-    MPI_Init(NULL, NULL);
+    // Enable multithread
+    MPI_Init_thread(nullptr, nullptr, MPI_THREAD_SERIALIZED, nullptr);
 
     // Get the number of processes
     int world_size;
@@ -271,21 +32,28 @@ int main(int argc, char** argv) {
     int currank;
     MPI_Comm_rank(MPI_COMM_WORLD, &currank);
 
-    // Get the name of the processor
+/*    // Get the name of the processor
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
+    MPI_Get_processor_name(processor_name, &name_len);*/
 
     // Construct DBG
     string folderPath = "./input/";
     string currentFile = "file";
     VertexList *vertexList = new VertexList;
+    SetOfID *tangleList = new SetOfID;
     EdgeList *edgeList = new EdgeList;
+
+    /*VertexList *vertexList4Others = new VertexList;
+    EdgeList *edgeList4Others = new EdgeList;
+    SetOfID *tangleList4Others = new SetOfID;*/
     int k = 3;
     int readNum = 1024 * 1024 * 1024;
     int numToProcess = readNum / world_size;
     int beginIndex =  currank * numToProcess;
     int endIndex = currank+1==world_size? readNum-1: beginIndex+numToProcess-1;
+
+    // Some temporary variables
     string *curread = new string;
     ReadId *readId = new ReadId;
     size_t readpos;
@@ -296,6 +64,24 @@ int main(int argc, char** argv) {
     VertexId *tmpVertexId1 = new VertexId;
     VertexId *tmpVertexId2 = new VertexId;
     KMERPOS_t edgeMode = NOT_INCLUDE_KMER;
+
+    // 开启线程
+    pthread_t sender_tid, receiver_tid;
+    RecvArg *recvArg = new RecvArg;
+    recvArg->vertexList = vertexList;
+    recvArg->edgeList = edgeList;
+    recvArg->tangleList = tangleList;
+
+    if (pthread_create(&sender_tid, nullptr, senderRunner, nullptr) != 0) {
+        cerr << "Cannot create sender thread";
+        exit(1);
+    }
+
+    if (pthread_create(&receiver_tid, nullptr, receiverRunner, recvArg) != 0) {
+        cerr << "Cannot create receiver thread";
+        exit(1);
+    }
+
     while (!(*curread = getNextRead(currank, world_size, readNum, folderPath, &readpos)).empty()) {
         // 传送read的文件名和文件指针到目标机器
         int read4Rank = currank;
@@ -327,15 +113,38 @@ int main(int argc, char** argv) {
             else if (i == end - 1) edgeMode = END_KMER;
             else edgeMode = INCLUDE_KMER;
 
-            addVertex(vertexList, tmpKMinusMer1, tmpVertexId1, 0, 0, ISOLATE_VERTEX);
-            addVertex(vertexList, tmpKMinusMer2, tmpVertexId2, 0, 0, ISOLATE_VERTEX);
-            addNewEdge(edgeList, tmpKMer, tmpEdgeId, *tmpVertexId1, *tmpVertexId2, *readId, edgeMode);
-            addOutEdge(vertexList,*tmpVertexId1, *tmpEdgeId);
-            addInEdge(vertexList,*tmpVertexId2, *tmpEdgeId);
+            *tmpEdgeId = getId(string(tmpKMer));
+            *tmpVertexId1 = getId(string(tmpKMinusMer1));
+            *tmpVertexId2 = getId(string(tmpKMinusMer2));
+
+            // 创建开始Vertex
+            if (*tmpVertexId1 % world_size != currank) {
+                requestOtherRanksToStoreVertex(currank, world_size, *tmpVertexId1, *tmpEdgeId, HEAD_VERTEX);
+            } else {
+                if ((addVertex(vertexList, tmpKMinusMer1, 0, *tmpEdgeId, HEAD_VERTEX) & MULTI_OUT_DEGREE) == MULTI_OUT_DEGREE){
+                    tangleList->insert(*tmpVertexId1);
+                }
+            }
+
+            // 创建终止Vertex
+            if (*tmpVertexId2 % world_size != currank) {
+                requestOtherRanksToStoreVertex(currank, world_size, *tmpVertexId2, *tmpEdgeId, TAIL_VERTEX);
+            } else {
+                if ((addVertex(vertexList, tmpKMinusMer2, *tmpEdgeId, 0, TAIL_VERTEX) & MULTI_OUT_DEGREE) == MULTI_OUT_DEGREE)
+                {
+                    tangleList->insert(*tmpVertexId2);
+                }
+            }
+
+            // 创建Edge
+            if (*tmpEdgeId % world_size != currank)
+                requestOtherRanksToStoreEdge(currank, world_size, tmpKMer, *tmpEdgeId, *readId, edgeMode);
+            else
+                addNewEdge(edgeList, tmpKMer, tmpEdgeId, *tmpVertexId1, *tmpVertexId2, *readId, edgeMode);
         }
     }
-    // TODO: sync kmer and kminusmer.
     // TODO: delete the variables above.
+
     // Finalize the MPI environment.
     MPI_Finalize();
     return 0;
